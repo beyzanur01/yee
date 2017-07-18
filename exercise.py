542f1d201f1abad2fa492dde41405f7661fce27a
beyzanur01 <beyzanur.cay.01@gmail.com>
2017-07-18 15:38:29 -0400

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,10 @@
+answer1 = input ("give me a number ")
+answer2 = input ("give me another ")
+multiplication = (answer1 * 3)
+addition = (answer1 + answer2)
+
+if answer1 == answer2:
+    print (multiplication)
+else:
+    print (addition)
+        

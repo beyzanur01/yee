8311c36faf2b94b717a3162804591bfba649017d
beyzanur01 <beyzanur.cay.01@gmail.com>
2017-07-18 15:38:06 -0400

    Add files via upload

---
+++ beyza_dontdeletepls (2).py
@@ -0,0 +1,175 @@
+#number = 2
+#adding comments
+"""addition = 40 + 60
+subtraction = 10 - 3
+multiplication = 10 * 8
+division = 30 / 6
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)"""
+
+"""exponent = 5**7
+modulo = 15%4 """
+
+lizards = 50 / 2 *3
+print (lizards)
+
+nice = (lizards + 1)
+print  (nice)
+
+"""division = 50 / 100
+multiplication = 0.5 * 13
+print (multiplication)"""
+
+"""division =  56.5 / 100
+print (division)
+multiplication = 0.565 * 15
+print (multiplication)"""
+
+"""addition = 8.475 + 56.5
+print (addition)"""
+
+"""tax = 50 / 100 * 13
+print (tax)
+
+tip = (50 + 6.5) / 100 * 15
+print (tip)
+
+total = 56.5 + 8.475
+print (total)"""
+
+"""tax = 0.13 #code more efficiently pls
+tip = 0.15
+cost = 50
+
+subtotal = cost + (cost * tax)
+total = subtotal + (subtotal * tip)
+print (total)"""
+
+"""name = "kakyoin"
+print (name[1])"""
+
+"""jojo = "jotaro"
+print (len(jojo)) #this can be used for all data types
+
+jojo = "JOTARO"
+print (jojo.lower()) #if there is a dot it only works with strings
+
+jojo = "jotaro"
+print (jojo.upper ())"""
+
+#print ( "star" + " " + "platinum" )
+
+"""answer = input ("is that a jojo reference dude? ")
+print (answer)"""
+
+"""bro1 = 1
+bro2 = 5
+
+bro1 = = bro2 #equals to
+bro1 != bro2 #inequal
+bro1 < bro2 #less than
+bro1 > bro2 # more than
+bro1 <= bro2 #is less than or equal to
+bro1 >= bro2 #is more than or equal to"""
+
+"""v1 = (20-10) > 15 
+v2 = (10+17) == 3**16
+v3 = 1**2 <= -1
+v4= 40*4 >= -4
+v5= 100 != 10**2
+
+v1 = False
+v2 = False
+v3 = False
+v4 = True
+v5 = False"""
+
+# "and" "or" "not" are boolean operators
+
+"""v1 = -(-(-(-2))) == -2 and (4 >= 16**0.5)
+v2 = 19%4 != 300/10/10 and False
+v3 = -(1**2)<2**0 and 10%10 <= 20-10*2
+
+v1=  False
+v2 = False
+v3 = True"""
+
+"""v1= 2**3 == 108%100 or "Apple" == "Orange"
+v2= 100**0.5 >=  50 or False
+v3= 1**100 == 100**1 or 3*2*1 != 3+2+1
+
+v1= True
+v2 = False
+v3 = False"""
+
+"""v1= not 3**4 < 4**3
+v2 = not 10%3 <= 10%2
+v3 = not not False
+
+v1= True
+v2= True
+v3= False"""
+
+"""name = "kakyoin"
+
+if 10 < 7 :
+    print ("yay")
+elif  name == "kakyoin":
+    print ("rero")
+else:
+    print (":c")"""
+
+
+if 10**2 > 10**3 and False:
+    print ("ok")
+elif 3*2 == 1*6:
+    print (True)
+else:
+    print ("bye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
